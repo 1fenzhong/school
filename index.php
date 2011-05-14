854d5779449459da1e505ab88b07a669360a59cd
L42y <423300@gmail.com>
2011-05-14 20:13:47 +0800

    add article url attr

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>" url="<?php the_permalink() ?>">
 
 			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
