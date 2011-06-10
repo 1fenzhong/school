acdc5d62d4371a4bd4831eb12d4a1b3d59e71bf9
L42y <423300@gmail.com>
2011-06-10 23:52:04 +0800

    about us page

---
+++ page-aboutus.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+
+<section id="content">
+
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+    <div id="aboutus">
+
+        <div class="about"><?php the_content(); ?></div>
+        
+        <div id="we">
+            <div class="me">
+                <h2 class="name"><?php the_author_meta( 'display_name', 1 ); ?></h2>
+                <div class="avatar"><?php echo get_avatar( 1, 200 ); ?></div>
+                <div class="intro"><?php the_author_meta( 'description', 1 ); ?></div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+        <?php comments_template(); ?>
+        
+    </div>
+
+    <?php endwhile; endif; ?>
+    
+</section>
+
+<?php get_footer(); ?>
