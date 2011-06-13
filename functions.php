39c176f01467f49846e9b20e082a490220aeda50
L42y <423300@gmail.com>
2011-06-13 22:29:48 +0800

    log post type & log tag

--- functions.php
+++ functions.php
@@ -89,4 +89,40 @@ class description_walker extends Walker_Nav_Menu
   }
 }
 
+// log tag
+register_taxonomy('log_tag', 'log', array(
+  'label' => 'Log 标签',
+  'singular_label' => 'Log 标签',
+  'public' => true,
+  'show_tagcloud' => false,
+  'query_var' => true,
+  'hierarchical' => false,
+  'rewrite' => array(
+    'slug' => 'log/tag',
+    'with_front' => 'false'
+  ),
+));
+
+//product post type
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+  register_post_type( 'log',
+    array(
+      'labels' => array(
+        'name' => __( 'Log' ),
+        'singular_name' => __( 'Log' ),
+        'add_new' => __('生产 Log'),
+        'add_new_item' => __('生产 Log'),
+        'rewrite' => array('slug' => '$slug'),
+                        ),
+      'menu_position' => 5,
+      'publicly_queryable' => true,
+      'capability_type' => 'post',
+      'query_var' => true,
+      'public' => true,
+      'has_archive' => true,
+      'supports' => array('title', 'editor', 'comments', 'revisions', 'post-formats'),
+      'taxonomies' => array('log_tag')));
+}
+
 ?>
