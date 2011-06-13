6d7ad059ab04b7db64d8e69c827c892898d6baab
L42y <423300@gmail.com>
2011-06-14 00:35:46 +0800

    fun

--- _/inc/log-meta.php
+++ _/inc/log-meta.php
@@ -4,7 +4,7 @@
         </div>
                                 
 	<span class="byline author vcard">
-            <span class="fn"><?php the_author(); ?></span> 又在碎碎念，
+            <span class="fn"><?php the_author(); ?></span> 又在碎碎念 
 	</span>
 	<?php comments_popup_link('没人理会', '被批评1次', '被批评%次', 'comments-link', ''); ?>
 </footer>
