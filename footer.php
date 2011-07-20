61cbd543b65aceb96927a608d71398eec04683d3
L42y <423300@gmail.com>
2011-07-20 23:18:12 +0800

    哟

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 		<footer id="footer" class="source-org vcard copyright">
                         <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container' => '', 'after' => '', 'walker' => new description_walker() ) ); ?>
 
-			<small>&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?></small>
+			<small>&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?> <a href="http://www.miibeian.gov.cn/">粤ICP备11053315号</a></small>
 		</footer>
 
         </div>
